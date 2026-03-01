001aaa6748cc4d033e32edc10948e70cb172e090
Kleidi Bujari <mail@4kb.net>
2026-03-01 12:27:16 -0800

    disable vc-mode

--- init.el
+++ init.el
@@ -36,6 +36,9 @@
         auto-save-default nil
         make-backup-files nil)
 
+;; Disable vc-mode entirely
+(setopt vc-handled-backends nil)
+
 ;; Conditionally enable HTTP proxy before package init
 (when-let* ((proxy (getenv "FWDPROXY")))
   (setopt url-proxy-services
